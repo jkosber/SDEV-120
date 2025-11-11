79197370a0cddbd4212c11b14e325dfa7da48018
jkosber <jadonkosberg@gmail.com>
2025-11-11 18:30:02 -0500

    11/11 Homework in SDEV120
    Homework

---
+++ SDEV120/Class3InClassPractice.py
@@ -0,0 +1,11 @@
+# This program is going to ask the user to enter their name
+
+first_name = input ("please enter your first name: ")
+print(f"Welcome Your first name is {first_name}")
+
+# age
+
+height = input
+width = input ("
+total_height = height + width
+print(f"Your height is {total_height}")
